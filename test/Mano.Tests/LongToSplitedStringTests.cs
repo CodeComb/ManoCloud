638b42c7acebf7c5969327ff38d4892c37aa8075
あまみや ゆうこ <1@1234.sh>
2016-03-24 12:42:19 +0800

    Make test cases beautiful

--- test/Mano.Tests/LongToSplitedStringTests.cs
+++ test/Mano.Tests/LongToSplitedStringTests.cs
@@ -10,15 +10,15 @@ namespace Mano.Tests
     public class LongToSplitedStringTests
     {
         [Theory]
+        [InlineData(0, "0")]
+        [InlineData(12, "12")]
+        [InlineData(123, "123")]
+        [InlineData(1234, "1,234")]
+        [InlineData(12345, "12,345")]
         [InlineData(123456, "123,456")]
         [InlineData(57438295, "57,438,295")]
         [InlineData(157438295, "157,438,295")]
         [InlineData(1157438295, "1,157,438,295")]
-        [InlineData(0, "0")]
-        [InlineData(123, "123")]
-        [InlineData(12, "12")]
-        [InlineData(1234, "1,234")]
-        [InlineData(12345, "12,345")]
         public void SplitTest(long num, string ans)
         {
             Assert.Equal(ans, num.ToSplitedString());
