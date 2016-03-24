e55712a04accf35f81e8116e7ae531cfbc5992e0
あまみや ゆうこ <1@1234.sh>
2016-03-24 21:29:11 +0800

    Update skills

--- src/Mano/Controllers/ApiController.cs
+++ src/Mano/Controllers/ApiController.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
 using Microsoft.AspNet.Mvc;
+using Microsoft.Data.Entity;
 using Newtonsoft.Json;
 using Mano.Models;
-using Mano.Parser.Models;
 
 namespace Mano.Controllers
 {
@@ -23,6 +23,48 @@ namespace Mano.Controllers
                 if (DB.Commits.SingleOrDefault(y => y.Id == x.Id) == null)
                     DB.Commits.Add(x);
             DB.SaveChanges();
+            lock(this)
+            {
+                var emails = DB.Emails
+                    .Where(x => x.Verified && x.UserId == project.UserId)
+                    .Select(x => x.EmailAddress)
+                    .ToList();
+                var tech = DB.Extensions
+                    .Where(x => x.Type == TechnologyType.编程语言 || x.Type == TechnologyType.序列化格式)
+                    .ToDictionary(x => x.Id);
+                var statistics = DB.Commits
+                    .Include(x => x.Changes)
+                    .ThenInclude(x => x.Commit)
+                    .Where(x => x.ProjectId == id)
+                    .SelectMany(x => x.Changes)
+                    .GroupBy(x => tech[Path.GetExtension(x.Path)])
+                    .Select(x => new { Key = x.Key, Count = x.Sum(y => y.Additions + y.Deletions), Begin = x.Min(y => y.Commit.Time) })
+                    .ToList();
+                foreach (var x in statistics)
+                {
+                    if (DB.Skills.Where(y => y.UserId == project.UserId && y.Verified && y.Title == x.Key.Technology).Count() > 0)
+                    {
+                        var skill = DB.Skills.Where(y => y.UserId == project.UserId && y.Verified && y.Title == x.Key.Technology).Single();
+                        skill.Count = x.Count;
+                        skill.Begin = x.Begin;
+                    }
+                    else
+                    {
+                        DB.Skills.Add(new Skill
+                        {
+                            Begin = x.Begin,
+                            Title = x.Key.Technology,
+                            Count = x.Count,
+                            Unit = "行",
+                            Type = x.Key.Type,
+                            UserId = project.UserId,
+                            Verified = true,
+                            UpdateFromGit = true
+                        });
+                    }
+                }
+                DB.SaveChanges();
+            }
             return Content("ok");
         }
     }
---
+++ src/Mano/Models/Commit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mano.Models
+{
+    public class Commit : Parser.Models.Commit
+    {
+        [ForeignKey("Project")]
+        public Guid ProjectId { get; set; }
+
+        public virtual Project Project { get; set; }
+    }
+}
